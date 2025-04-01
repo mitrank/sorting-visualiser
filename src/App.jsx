83911df7c15ee396d7e9ef880d07c889cc38acfb
mitrank <mitrankshah@yahoo.com>
2025-04-02 02:13:05 +0530

    shadcn integration slider input for array size

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import SortingInput from "./components/SortingInput";
 import SortingWindow from "./components/SortingWindow";
 
 function App() {
+  const [arraySize, setArraySize] = useState(0);
+
+  useEffect(() => {
+    console.log(arraySize);
+  }, [arraySize]);
+
   return (
     <div className="App">
       <Header />
       <div className="flex gap-2 justify-between w-full h-full">
-        <SortingInput />
+        <SortingInput setArraySize={setArraySize} />
         <SortingWindow />
       </div>
     </div>
--- src/components/SortingInput.jsx
+++ src/components/SortingInput.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { Slider } from "@/components/ui/slider";
 
-const SortingInput = () => {
+const SortingInput = ({ setArraySize }) => {
   return (
     <div className="w-md h-full rounded-md border border-[#ffa600]-200 bg-[#003f5c]">
-      SortingInput
+      <div className="flex flex-col justify-center items-center mt-4">
+        <h4 className="text-lg font-semibold mb-2">Array Size</h4>
+        <div className="flex flex-row w-full justify-center">
+          <span className="mr-2">10</span>
+
+          <Slider
+            defaultValue={[50]}
+            min={10}
+            max={500}
+            step={1}
+            className="w-[60%] dark"
+            onValueChange={(val) => {
+              setArraySize(val);
+            }}
+          />
+          <span className="ml-2">500</span>
+        </div>
+      </div>
     </div>
   );
 };
